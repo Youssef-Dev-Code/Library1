86df35961e1ab4d0d3dc8c8c907dfea3cbc7adf9
Youssef-Dev-Code <youssefazaiez21@gmail.com>
2022-11-30 13:10:26 +0100

    modified:   File1.c

--- File1.c
+++ File1.c
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[])
 {
     system("mkdir /usr/Import"); // creating directory to save data
     Book *Head_Book = NULL;
-    Theme *Head_Theme[MAX_THEMES];
+    Theme *Theme_List[MAX_THEMES];
     Author *Head_Author = NULL;
     __uint8_t x;
     Bool Test = True;
@@ -15,10 +15,19 @@ int main(int argc, char const *argv[])
         scanf("%c", &x);
         switch (x)
         {
+        case 0:
+            Test = False;
+            break;
         case 1:
-        
+            Add_Book(&Head_Book, &Head_Author);
             break;
         case 2:
+            Theme_Add(Theme_List);
+            break;
+        case 3:
+            Author_Create(&Head_Author);
+            break;
+        case 4:
             
             break;
         default:
